7b4317f9dd5fe0cc0b9f5f5888b2909f9bcebed6
Loris Campanile <Loris1634@gmail.com>
2018-05-17 23:11:56 +1000

    Update index.js

--- channel40-new/registration/role/index.js
+++ channel40-new/registration/role/index.js
@@ -1,46 +1,26 @@
 {
-  "Grid": {
-    "rows": 2,
-    "cols": 3,
+    "component": "Box",
+    "props": {
+        "flexDirection": "row",
+        "alignItems": "center",
+        "justifyContent": "center",
+        "flex": 1,
+        "height": "100%"
+     },
     "children": [
       {
-        "h2": {
-          "position": [0, 1],
-          "children": "Select your role",
-          "style": {
-            "color": "PROJECT.PRI_COLOR",
-            "text-align" : "center"
-          }
-        }
-      },
-      {
-        "GennyButton": {
-          "position": [1, 1],
+         "component": "Button",
+         "props": {
           "buttonCode": "BTN_SELECT_ROLE_BUYER",
-          "value": {},
-          "style": {
-            "margin": "10px"
-          },
-          "buttonStyle": {
-            "background": "PROJECT.PRI_COLOR"
-          },
           "children": "I am a Freight Owner"
         }
       },
       {
-        "GennyButton": {
-          "position": [1, 1],
+         "component": "Button",
+         "props": {
           "buttonCode": "BTN_SELECT_ROLE_SELLER",
-          "value": {},
-          "style": {
-            "margin": "10px"
-          },
-          "buttonStyle": {
-            "background": "PROJECT.PRI_COLOR"
-          },
           "children": "I am a Driver"
         }
       }
     ]
-  }
 }
