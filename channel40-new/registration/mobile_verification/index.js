ee1f44e3807d3b266ac9d48a025a9d01045f566e
Loris Campanile <Loris1634@gmail.com>
2018-05-21 10:05:22 +1000

    Create index.js

---
+++ channel40-new/registration/mobile_verification/index.js
@@ -0,0 +1,26 @@
+{
+  "layout": {
+    "title": "Registration",
+    "appColor": "dark"
+  },
+  "children": [
+      {
+      "component": "Container",
+      "props": {
+        "flexDirection": "column",
+        "alignItems": "center",
+        "justifyContent": "center",
+        "flex": 1,
+        "height": "100%"
+      },
+      "children": [
+        {
+          "component": "Form",
+          "props": {
+            "questionGroupCode": "QUE_MOBILE_VERIFICATION_GRP"
+          }
+        }
+      ]
+    }
+  ]
+}
